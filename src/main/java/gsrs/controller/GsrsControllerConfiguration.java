0890c1d97e69b63847ea799433c01d97715e7b4d
katzelda <daniel.katzel@nih.gov>
2020-10-22 13:19:21 -0400

    added catch all error handling for runtime exceptions to now support error_response query param

--- src/main/java/gsrs/controller/GsrsControllerConfiguration.java
+++ src/main/java/gsrs/controller/GsrsControllerConfiguration.java
@@ -1,14 +1,17 @@
 package gsrs.controller;
 
 import gov.nih.ncats.common.util.CachedSupplier;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.WebRequest;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +27,10 @@ public class GsrsControllerConfiguration {
     private static boolean isValidErrorCode(int askedForStatus) {
         return askedForStatus >=400 && askedForStatus< 600;
     }
-
+    public int overrideErrorCodeIfNeeded(int defaultStatus, WebRequest request){
+        String value =request.getParameter(errorCodeParameter);
+        return overrideErrorCodeIfNeeded(defaultStatus, Collections.singletonMap(errorCodeParameter, value));
+    }
     public int overrideErrorCodeIfNeeded(int defaultStatus, Map<String, String> queryParameters){
         //GSRS-1598 force not found error to sometimes be a 500 instead of 404
         //if requests tells us
@@ -56,6 +62,23 @@ public class GsrsControllerConfiguration {
         int status = overrideErrorCodeIfNeeded(404, queryParameters);
         return new ResponseEntity<>( createStatusJson("not found", status), HttpStatus.valueOf(status));
 
+    }
+    public ErrorInfo createErrorStatusBody(Throwable t, int status,  WebRequest request){
+        int statusToUse = overrideErrorCodeIfNeeded(status, request);
+        Object body = createStatusJson("not found", status);
+        return ErrorInfo.builder()
+                            .body(body)
+                            .status(HttpStatus.valueOf(statusToUse))
+                            .build();
+
+
+    }
+
+    @Data
+    @Builder
+    public static class ErrorInfo{
+        private Object body;
+        private HttpStatus status;
     }
 
     private static Object getError(Throwable t, int status){
