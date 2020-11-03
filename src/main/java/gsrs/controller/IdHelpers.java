956c9eda7118a567562c26ba33571d742e343737
katzelda <daniel.katzel@nih.gov>
2020-11-03 16:04:26 -0500

    tests and bug fixes for idHelper

--- src/main/java/gsrs/controller/IdHelpers.java
+++ src/main/java/gsrs/controller/IdHelpers.java
@@ -5,14 +5,20 @@ package gsrs.controller;
  */
 public enum IdHelpers implements IdHelper {
     /**
-     * An ID that is a number.
+     * An ID that is a only digits.
      */
-    NUMBER("[0-9]+", "[^0-9]+"),
+    NUMBER("[0-9]+", "^\\d*[a-zA-Z][a-zA-Z\\d]*"),
     STRING_NO_WHITESPACE("\\S+"),
     /**
      * An ID that is a UUID.
      */
-    UUID( "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}"),
+    UUID( "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}",
+            //I don't think lookaheads with curly braces are supported in Java 8?
+            // so for now just make a a-z0-9 check that's not the exact length as uuid
+//            "(?![a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]-[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]-[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]-[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]-[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9])"
+//            "(?![a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})"
+            "([a-zA-Z0-9\\-]{1,35})|([a-zA-Z0-9\\-]{37,})"
+    ),
     CUSTOM(".*")
     ;
 
@@ -24,7 +30,7 @@ public enum IdHelpers implements IdHelper {
         this.notRegex = notRegex;
     }
     IdHelpers(String regex) {
-        this(regex, "\\.+");
+        this(regex, ".+");
     }
 
     @Override
