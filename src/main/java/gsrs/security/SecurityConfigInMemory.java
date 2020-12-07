2366ab92e3e18d463b11d620e3e8c3be66086f09
katzelda <daniel.katzel@nih.gov>
2020-12-07 13:40:45 -0500

    kinda working authentication

--- src/main/java/gsrs/security/SecurityConfigInMemory.java
+++ src/main/java/gsrs/security/SecurityConfigInMemory.java
@@ -4,6 +4,7 @@ import ix.core.models.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +15,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true,
@@ -26,42 +30,64 @@ public class SecurityConfigInMemory extends WebSecurityConfigurerAdapter {
     @Autowired
     LegacyGsrsAuthenticationProvider legacyGsrsAuthenticationProvider;
 
-    @Autowired
-    LegacyAuthenticationFilter legacyAuthenticationFilter;
+//    @Autowired
+//    LegacyAuthenticationFilter legacyAuthenticationFilter;
+    @Bean
+    public LegacyAuthenticationFilter legacyAuthenticationFilter(){
+        return new LegacyAuthenticationFilter();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-        .antMatchers("/login*").permitAll()
+        http
+                .authorizeRequests()
+//                .antMatchers(HttpMethod.DELETE, "/api/*").authenticated()
+//                .antMatchers(HttpMethod.PUT, "/api/*").authenticated()
+//                .antMatchers(HttpMethod.POST, "/api/*").authenticated()
+//        .antMatchers("/login*").permitAll()
 
-        .anyRequest().permitAll()
-//                .and().addFilterBefore(legacyAuthenticationFilter, BasicAuthenticationFilter.class)
-                .and().authenticationProvider(legacyGsrsAuthenticationProvider)
+        .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(legacyAuthenticationFilter(), BasicAuthenticationFilter.class)
+//                .and()
+//                .authenticationProvider(legacyGsrsAuthenticationProvider)
 //                .and().httpBasic()
 //                .and().formLogin();
+//                .and()
         .csrf().disable()
+        .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint());
 
         ;
+    }
+    @Bean
+    public AuthenticationEntryPoint unauthorizedEntryPoint() {
+        return (request, response, authException) -> response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return NoOpPasswordEncoder.getInstance();
 //    }
-    @Bean
-    public UserDetailsService userDetailsService() {
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+////        User.UserBuilder users = User.builder();
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.builder().username("user").password("{noop}password").roles(Role.Query.name()).build());
+//        manager.createUser(User.builder().username("admin").password("{noop}admin").roles( Role.Admin.name()).build());
+//        return manager;
+//
+//    }
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(legacyGsrsAuthenticationProvider);
+}
 
-//        User.UserBuilder users = User.builder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.builder().username("user").password("{noop}password").roles(Role.Query.name()).build());
-        manager.createUser(User.builder().username("admin").password("{noop}admin").roles( Role.Admin.name()).build());
-        return manager;
 
-    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.eraseCredentials(false);
-        auth.userDetailsService(userDetailsService());
+//        auth.userDetailsService(userDetailsService());
 //        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles(Role.Admin.name());
 //        auth.inMemoryAuthentication().withUser("user1").password("{noop}pass").roles(Role.Query.name());
 //        auth.inMemoryAuthentication().withUser("user2").password("{noop}pass").roles(Role.SuperUpdate.name());
